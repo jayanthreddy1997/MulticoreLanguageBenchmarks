d843110b6a05b960e913ad3f0e4f207ad57950f8
Jayanth Reddy <njkreddy1997@gmail.com>
2023-04-05 18:08:16 -0400

    openmp mat vec mult

--- openmp/matrix_vector_mult/main.cpp
+++ openmp/matrix_vector_mult/main.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-void matrix_vec_mult_sequential(int m, int n, float** A, float** v, float** res) {
+void matrix_vec_mult_sequential(int m, int n, float** A, float* v, float* res) {
     for (int i=0; i<m; i++) {
-        res[i][0] = 0.0;
+        res[i] = 0.0;
         for (int j=0; j<n; j++) {
-            res[i][0] += A[i][j] * v[n][0];
+            res[i] += A[i][j] * v[n];
         }
     }
 }
 
-void matrix_vec_mult_parallel(int m, int n, float** A, float** v, float** res, int thread_count) {
+void matrix_vec_mult_parallel(int m, int n, float** A, float* v, float* res, int thread_count) {
     int i, j;
 #pragma omp parallel for num_threads(thread_count) \
         default(none) private(i, j) shared(A, v, res, m, n)
     for (i=0; i<m; i++) {
-        res[i][0] = 0.0;
+        res[i] = 0.0;
         for (j=0; j<n; j++) {
-            res[i][0] += A[i][j] * v[n][0];
+            res[i] += A[i][j] * v[n];
         }
     }
 }
@@ -38,17 +38,29 @@ float** get_matrix(int m, int n, bool random_init=false) {
     return mat;
 }
 
+float* get_vector(int n, bool random_init=false) {
+    float* vec = new float[n];
+
+    if (random_init) {
+        for (int i=0; i<n; i++) {
+                vec[i] = (float)(rand()) / (float)(RAND_MAX);
+        }
+    }
+    return vec;
+}
+
+
 int main() {
     // m*n Matrix multiplication with n*1 vector
-    int m = 1000;
-    int n = 1000;
+    int m = 10000;
+    int n = 100000;
     bool run_parallel = false;
     int n_threads = 10;
 
     printf("Initializing matrices.\n");
     float** A = get_matrix(m, n, true);
-    float** v = get_matrix(n, 1, true);
-    float** res = get_matrix(m, 1, false);
+    float* v = get_vector(n, true);
+    float* res = get_vector(m, false);
 
     printf("Running Matrix Vector multiplication.\nMatrix Size: %d x %d\nParallel Execution: %d\nThread Count: %d\n",
            m, n, run_parallel, run_parallel?n_threads:1);

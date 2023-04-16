fc91caf5ce9a9be6ab6866890e7ea1f327be24ef
Manas Vegi <manasvegi@gmail.com>
2023-04-16 19:03:09 -0400

    Change timers in rust to Instant

--- rust/sorting/parallelrustsort/src/main.rs
+++ rust/sorting/parallelrustsort/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use rand::prelude::*;
 use rayon::prelude::*;
-use easybench::{bench_env};
+use std::time::Instant;
 fn add_random(list : &mut Vec<i64>, rng : &mut ThreadRng) {
     let y = ((rng.gen::<f64>() * 200.0) - 100.0) as i64;
     list.push(y);
@@ -16,5 +16,9 @@ fn main() {
         add_random(&mut vec, &mut rand_gen);
     }
     rayon::ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
-    println!("sort:    {}", bench_env(vec, |vec| vec.par_sort_unstable()));
+
+    let now = Instant::now();
+    vec.par_sort_unstable();
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.6?}", elapsed);
 }
--- rust/sorting/rustsort/src/main.rs
+++ rust/sorting/rustsort/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use rand::prelude::*;
-use easybench::{bench_env};
+use std::time::Instant;
 fn add_random(list : &mut Vec<i64>, rng : &mut ThreadRng) {
     let y = ((rng.gen::<f64>() * 200.0) - 100.0) as i64;
     list.push(y);
@@ -13,9 +13,8 @@ fn main() {
     for _ in 1..num {
         add_random(&mut vec, &mut rand_gen);
     }
-    println!("sort:    {}", bench_env(vec, |vec| vec.sort()));
-
-    // let contents = fs::read_to_string(filepath)
-    //     .expect("Should have been able to read the file");
-    // println!("{}", contents);
+    let now = Instant::now();
+    vec.sort();
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.6?}", elapsed);
 }

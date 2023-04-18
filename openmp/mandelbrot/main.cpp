966ef6ef68585cbf316716b20dca8cd25c860ece
Jayanth Reddy <njkreddy1997@gmail.com>
2023-04-18 01:27:21 -0400

    omp serial

--- openmp/mandelbrot/main.cpp
+++ openmp/mandelbrot/main.cpp
@@ -13,8 +13,8 @@ double X_MIN = -2.0;
 double X_MAX = 1;
 double Y_MIN = -1.5;
 double Y_MAX = 1.5;
-int IMG_HEIGHT = 1024;
-int IMG_WIDTH = 1024;
+int IMG_HEIGHT = 4096;
+int IMG_WIDTH = 4096;
 int MAX_ITERATIONS = 100;
 double COMPUTATION_LIMIT = 4.0; // Absolute value after which the mandel set is assumed to diverge
 
@@ -23,47 +23,60 @@ unsigned char double_to_unsignedchar(const double d)
     return round(std::max(std::min(1., d), 0.) * 255);
 }
 
-void write_matrix_to_uint8(int* data, int h, int w, std::vector<uint8_t> &image)
+void write_matrix_to_uint8(double* data, int h, int w, std::vector<uint8_t> &image)
 {
     image.resize(w * h * 4, 0);
 
-    int loc;
-    unsigned i = 0;
-    for (unsigned hi = 0; hi < h; ++hi)
+    unsigned i = 0, x;
+    for (int j=0; j<h*w; j++)
     {
-        for (unsigned wi = 0; wi < w; ++wi)
-        {
-            loc = (hi * w * 4) + (wi * 4);
-            image[loc + 0] = double_to_unsignedchar(data[i+0]);
-            image[loc + 1] = double_to_unsignedchar(data[i+1]);
-            image[loc + 2] = double_to_unsignedchar(data[i+2]);
-            image[loc + 3] = double_to_unsignedchar(1.0);
-            i += 3;
-        }
+        x = double_to_unsignedchar(data[j]);
+        image[i + 0] = x;
+        image[i + 1] = x;
+        image[i + 2] = x;
+        image[i + 3] = 255;
+        i += 4;
     }
 }
 
-void save_img(int* data, int h, int w, char* filename) {
+void save_img(double* data, int h, int w, char* filename) {
     std::vector<uint8_t> image;
     write_matrix_to_uint8(data, h, w, image);
-
     stbi_write_png(filename, w, h, 4, image.data(), w*4);
 }
 
 
-void mandelbrot_serial(int* out) {
+double mandel(double c_re, double c_im) {
+    int count = 0;
+    double z_re = 0.0;
+    double z_im = 0.0;
+    double z_re_new, z_im_new;
+    while (true) {
+        if (count >= MAX_ITERATIONS) {
+            return 1.0;
+        }
+        if ((z_re*z_re + z_im*z_im) > COMPUTATION_LIMIT) {
+            return 0.0;
+        }
+        z_re_new = z_re*z_re - z_im*z_im;
+        z_im_new = 2.0 * z_re * z_im;
+        z_re = z_re_new + c_re;
+        z_im = z_im_new + c_im;
+        count += 1;
+    }
+}
+
+void mandelbrot_serial(double* out) {
     double start = omp_get_wtime();
-//    float dx = (x1 - x0) / width;
-//    float dy = (y1 - y0) / height;
-//
-//    for (int j = 0; j < height; j++) {
-//        for (int i = 0; i < width; ++i) {
-//            float x = x0 + i * dx;
-//            float y = y0 + j * dy;
-//            int index = (j * width + i);
-//            output[index] = mandel(x, y, maxIterations);
-//        }
-//    }
+
+    double dx = (X_MAX - X_MIN) / IMG_WIDTH;
+    double dy = (Y_MAX - Y_MIN) / IMG_HEIGHT;
+
+    for (int i = 0; i < IMG_HEIGHT*IMG_WIDTH; i++) {
+        double x = X_MIN + (i % IMG_WIDTH) * dx;
+        double y = Y_MIN + (i / IMG_WIDTH) * dy;
+        out[i] = mandel(x, y);
+    }
     double end = omp_get_wtime();
     printf ("Mandelbrot serial execution time: %fs \n", end-start);
 }
@@ -82,15 +95,15 @@ int main() {
     int n_threads = 10;
     MAX_ITERATIONS = 100;
 
-    int* output = new int[IMG_HEIGHT * IMG_WIDTH];
-    memset(output, 0, IMG_HEIGHT * IMG_WIDTH * sizeof(int));
+    double* output = new double[IMG_HEIGHT * IMG_WIDTH];
+    memset(output, 0, IMG_HEIGHT * IMG_WIDTH * sizeof(double));
 
-    cout << "Running Mandelbrot in serial" << endl;
+    printf("Running Mandelbrot in serial\n");
     mandelbrot_serial(output);
-    save_img(output, IMG_HEIGHT, IMG_WIDTH, "mandelbrot_serial.png");
+    save_img(output, IMG_HEIGHT, IMG_WIDTH, "mandelbrot_omp_serial.png");
 
-//    memset(output, 0, IMG_HEIGHT * IMG_WIDTH * sizeof(int));
-//    cout << "Running Mandelbrot in parallel with " << n_threads << " threads" << endl;
-//    mandelbrot_parallel(n_threads);
-//    save_img(output, "mandelbrot_parallel.png");
+    memset(output, 0, IMG_HEIGHT * IMG_WIDTH * sizeof(double));
+    cout << "Running Mandelbrot in parallel with " << n_threads << " threads" << endl;
+    mandelbrot_parallel(n_threads);
+    save_img(output, IMG_HEIGHT, IMG_WIDTH,"mandelbrot_omp_parallel.png");
 }

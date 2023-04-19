0d2fdf1eb4d1466c18f2f369e9486d9319fff6da
Manas Vegi <manasvegi@gmail.com>
2023-04-18 20:50:47 -0400

    change to random file creation in comp sort

--- openmp/sorting/parallel_sort.cpp
+++ openmp/sorting/parallel_sort.cpp
@@ -5,15 +5,27 @@
 using namespace std;
 int main(int argv, char** argc) {
     int N = stoi(argc[1]);
-    int num_threads = stoi(argc[2]);
-    const char* filename = argc[3];
-    ifstream inp(filename);
+    char* mode = argc[2];
+    int num_threads = 1;
+    if (mode[0] == 'P') {
+        num_threads = stoi(argc[3]);
+    }
     vector<int> vec;
     vec.reserve(N);
-    int i;
-    while (inp >> i) {
-        vec.push_back(i);
+    srand(omp_get_wtime());
+    for (int i = 0; i < N; i++) {
+        vec.push_back(rand() % 201 - 100);// Generate a random number between -100 and 100
     }
-    omp_set_num_threads(num_threads);
-    __gnu_parallel::sort(vec.begin(), vec.end(), __gnu_parallel::quicksort_tag());
+    if (mode[0] == 'P') {
+        double start = omp_get_wtime();
+        omp_set_num_threads(num_threads);
+        __gnu_parallel::sort(vec.begin(), vec.end(), __gnu_parallel::quicksort_tag());
+        double end = omp_get_wtime();
+        cout << (end - start) << "s" << "\n";
+    } else {
+        double start= omp_get_wtime();
+        std::sort(vec.begin(), vec.end());
+        double end = omp_get_wtime();
+        cout << (end - start) << "s" << "\n";
+    }
 }
